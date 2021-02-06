35b9cd498aa75b825e6a05fb33575f0bfeb3d7f0
Lavrentev Stanislav <hvoinii@gmail.com>
2021-02-06 09:34:44 +0600

    create displayist and getitle methods

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -2,26 +2,61 @@ import React from "react";
 import restaurants from "../sample-restaurants";
 
 class Landing extends React.Component {
+  state = {
+    display: false,
+    title: "",
+    url: "",
+  };
+
+  displayList = () => {
+    const { display } = this.state;
+    this.setState({ display: !display });
+  };
+
+  getTitle = (restaurant) => {
+    const { title, url } = restaurant;
+    this.setState({ title, url, display: false });
+  };
+
+  goToRestaurant = () => {
+    console.log("Go to restaurant");
+  };
   render() {
     return (
       <div className="restaurant_select">
         <div className="restaurant_select_top">
-          <div className="restaurant_select_top-header font-effect-outline">
-            Выбери ресторан
+          <div
+            onClick={this.displayList}
+            className="restaurant_select_top-header font-effect-outline"
+          >
+            {this.state.title ? this.state.title : "Выбери ресторан"}
           </div>
           <div className="arrow_picker">
             <div className="arrow_picker-up"></div>
             <div className="arrow_picker-down"></div>
           </div>
         </div>
-        <div className="restaurant_select_bottom">
-          <ul>
-            {restaurants.map((restaurant) => {
-              return <li key={restaurant.id}>{restaurant.title}</li>;
-            })}
-          </ul>
-        </div>
-        <button>Перейти в ресторан</button>
+
+        {this.state.display ? (
+          <div className="restaurant_select_bottom">
+            <ul>
+              {restaurants.map((restaurant) => {
+                return (
+                  <li
+                    onClick={() => this.getTitle(restaurant)}
+                    key={restaurant.id}
+                  >
+                    {restaurant.title}
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        ) : null}
+
+        {this.state.title && !this.state.display ? (
+          <button onClick={this.goToRestaurant}>Перейти в ресторан</button>
+        ) : null}
       </div>
     );
   }
