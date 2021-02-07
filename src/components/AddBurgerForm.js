c91e635257ab37328fb00affeee3b06e640ff9a1
Lavrentev Stanislav <hvoinii@gmail.com>
2021-02-07 15:39:01 +0600

    create AddBurgerForm

---
+++ src/components/AddBurgerForm.js
@@ -0,0 +1,62 @@
+import React from "react";
+
+class AddBurgerForm extends React.Component {
+  nameRef = React.createRef();
+  priceRef = React.createRef();
+  statusRef = React.createRef();
+  descRef = React.createRef();
+  imageRef = React.createRef();
+
+  createBurger = (event) => {
+    event.preventDefault();
+
+    const burger = {
+      name: this.nameRef.current.value,
+      price: parseFloat(this.priceRef.current.value),
+      status: this.statusRef.current.value,
+      desc: this.descRef.current.value,
+      image: this.imageRef.current.value,
+    };
+    console.log(burger);
+  };
+
+  render() {
+    return (
+      <form className="burger-edit" onSubmit={this.createBurger}>
+        <input
+          ref={this.nameRef}
+          name="name"
+          type="text"
+          placeholder="Name"
+          autoComplete="off"
+        />
+        <input
+          ref={this.priceRef}
+          name="price"
+          type="text"
+          placeholder="Price"
+          autoComplete="off"
+        />
+        <select
+          ref={this.statusRef}
+          className="status"
+          name="status"
+          type="text"
+        >
+          <option value="available">Доступно</option>
+          <option value="unavailable">Убрать из меню</option>
+        </select>
+        <textarea ref={this.descRef} name="desc" placeholder="Desc" />
+        <input
+          ref={this.imageRef}
+          name="image"
+          type="text"
+          placeholder="Image"
+          autoComplete="off"
+        />
+        <button type="submit">Добавить в меню</button>
+      </form>
+    );
+  }
+}
+export default AddBurgerForm;
--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -4,6 +4,7 @@ const NotFound = () => {
   return (
     <div>
       <h2>The Page Not Found</h2>
+      <a href="/">To main page</a>
     </div>
   );
 };
