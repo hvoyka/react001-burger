a289d1c5e521218f6a68284bfa2ba7306d95e8d9
Lavrentev Stanislav <hvoinii@gmail.com>
2021-02-07 19:52:23 +0600

    create Burger and show them in App

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import Header from "./Header";
 import Order from "./Order";
 import MenuAdmin from "./MenuAdmin";
+import sampleBurgers from "../sample-burgers";
+import Burger from "./Burger";
 
 class App extends React.Component {
   state = {
@@ -17,16 +19,33 @@ class App extends React.Component {
     //3. Записать новый объект burgers в state
     this.setState({ burgers });
   };
+  loadSampleBurgers = () => {
+    const burgers = { ...this.state.burgers, ...sampleBurgers };
+    this.setState({ burgers });
+  };
 
   render() {
     return (
       <div className="burger-paradise">
         <div className="menu">
           <Header title="Very Hot Burger" />
-          {/*  <Burger /> */}
+          <ul className="burgers">
+            {Object.keys(this.state.burgers).map((key) => {
+              return (
+                <Burger
+                  key={key}
+                  index={key}
+                  details={this.state.burgers[key]}
+                />
+              );
+            })}
+          </ul>
         </div>
         <Order />
-        <MenuAdmin addBurger={this.addBurger} />
+        <MenuAdmin
+          addBurger={this.addBurger}
+          loadSampleBurgers={this.loadSampleBurgers}
+        />
       </div>
     );
   }
---
+++ src/components/Burger.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+class Burger extends React.Component {
+  render() {
+    const { image, name, desc, price, status } = this.props.details;
+    return (
+      <li className="menu-burger">
+        <div className="image-container">
+          <img src={image} alt={name} />
+        </div>
+        <div className="burger-details">
+          <h3 className="burger-name">
+            {name}
+            <span className="price">{price} ₽</span>
+          </h3>
+          <p>{desc}</p>
+          <button className="buttonOrder">Заказать</button>
+        </div>
+      </li>
+    );
+  }
+}
+export default Burger;
